0079eeb66bb01e93b3a164f1fa67f249962d1783
RamaSandhy <rshandy07@gmail.com>
2024-11-21 16:29:14 +0700

    menambahkan object Gender untuk menyimpan daftar jenis kelamin

--- app/src/main/java/com/example/navigationwithmvvm/model/Gender.kt
+++ app/src/main/java/com/example/navigationwithmvvm/model/Gender.kt
@@ -2,7 +2,7 @@ package com.example.navigationwithmvvm.model
 
 import com.example.navigationwithmvvm.R
 
-object Gender {
+object Gender { //
     val ListGender = listOf(
         R.string.laki,
         R.string.wanita
